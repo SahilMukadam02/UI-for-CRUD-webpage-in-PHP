90f28402ae52de38c780856e64481f06de65ab6f
SahilMukadam02 <124427246+SahilMukadam02@users.noreply.github.com>
2023-10-17 14:52:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<div></div>
+ <h1>Create new Post</h1>
+ <h3>Title</h3>
+ <input type="text" id="newblog">
+ <h3>Content</h3>
+ <input type="text" id="content">
+ <h3>Category</h3>
+ <input type="checkbox" name="Social Issues" id="si"> Social Issues
+ <input type="checkbox" name="Science" id="sc"> Science
+ <input type="checkbox" name="Digital Marketing" id="dm"> Digital marketing
+<br>
+<br>
+ <input type="Submit"> 
+
+ </div>
+</html>
